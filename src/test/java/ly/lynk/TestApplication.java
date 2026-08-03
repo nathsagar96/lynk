906c94273198ad0a1b1a5cc999a8d7eaa4f6ed52
Sagar Nath <nathsagar96@gmail.com>
2026-08-03 23:11:44 +0530

    initial commit: spring boot 4.1.0 java 25 project setup

---
+++ src/test/java/ly/lynk/TestApplication.java
@@ -0,0 +1,11 @@
+package ly.lynk;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+    static void main(String[] args) {
+        SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
